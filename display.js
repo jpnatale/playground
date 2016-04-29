abe9f784a173b25fbe7060fb85a905b2a2153496
Joe Natale <jpnatale@gmail.com>
2016-04-29 11:46:37 -0400

    parsing update

--- display.js
+++ display.js
@@ -5,23 +5,47 @@ var data = {json: {username: "jpnatale", password: "d154f04ff209e6be596bc9f32ec8
 var lpBody = {}
 function callback (error, response, body) {
 		if (!error && response.statusCode == 200) {
-            lpBody = body
             console.log("Incoming Data...")
-            var jsonLibrary = JSON.parse(lpBody.library)
-            //console.log(jsonLibrary.items[0])
 
-            for (var i = 0; i < jsonLibrary.items.length - 1; i++) {
-            	if(!jsonLibrary.items[i].name){
-            		console.log(jsonLibrary.items[i])
-            	}
-            };
+            var jsonLibrary = JSON.parse(body.library)
+            
+            //Finds keys in object jsonLibrary
+            //console.log(Object.keys(jsonLibrary.categories[0]))
+
+            //console.log(jsonLibrary.categories[0])
+
+            jsonLibrary.lists[0].categoryIds.forEach(function(catId){
+                jsonLibrary.categories.forEach(function(category){
+                    if (catId == category.id) {
+                        //console.log(category.name)
+                        category.itemIds.forEach(function(itemId){
+                            //console.log(itemId.itemId)
+                            if (itemId.itemId == 96) {
+                                console.log(category.name)
+                            };
+                        })
+
+                    };
+                }) 
+            })
+
+            // for (var i = 0; i < jsonLibrary.lists[0].categoryIds.length; i++) {
+            //     jsonLibrary.categories.forEach(function(category){
+            //         if (jsonLibrary.lists[0].categoryIds[i] = category.id) {
+            //         console.log(category.name)}
+
+            //     })
+            // };
+
+            
+
+         
+
 
         }
         if (error){
         	console.log("There was an error: \(error)")
-
         }
-
 }
 
 request.post(url,data,callback)
